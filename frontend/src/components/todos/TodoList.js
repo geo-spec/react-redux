e8fbbc1b65dae803043f7bb2ae35eb9f23e3f9a7
Georgy <shilingeorgiy@yandex.ru>
2020-04-28 13:45:21 +0300

    1.1

--- frontend/src/components/todos/TodoList.js
+++ frontend/src/components/todos/TodoList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getTodos, deleteTodo } from '../../actions/todos';
-import Button from '@material-ui/core/Button';
+
 
 class TodoList extends Component {
   componentDidMount() {
