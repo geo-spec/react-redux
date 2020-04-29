412c4d986d4a04db0a544458066ae9aead6d7064
Georgy <shilingeorgiy@yandex.ru>
2020-04-29 18:28:48 +0300

    Add reduser

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import todos from './todos';
+import button_data_reducer from './button_data_reducer'
 
 export default combineReducers({
   form: formReducer,
-  todos
+  todos,
+  button_data_reducer
 });
